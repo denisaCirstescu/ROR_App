ae1e4d1c3a7ee6ceec93b35eeb3447ee4424511b
techclass <techclass@techclass-VirtualBox.(none)>
2013-07-21 15:10:15 +0300

    Having fun with ROR! :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '8b44c92b6546cbabcec216203e438ac1c0daa6a45987b80d25896b617fbafc8dabde816298cf88e288070104e76a984f99d71a832f645e87171b266ac0a63be4'
